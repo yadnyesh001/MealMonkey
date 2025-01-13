817a502dc4de4c00a32080a97ce550d6a27126d9
SahilKasare <cdsahilkasare@gmail.com>
2025-01-13 15:42:48 +0530

    update axios instance base URL to production environment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",  // Assuming your backend is running on localhost:5000/api
+  baseURL: "https://mealmonkey.onrender.com/",  // Assuming your backend is running on localhost:5000/api
   headers: {
     "Content-Type": "application/json",
   },
